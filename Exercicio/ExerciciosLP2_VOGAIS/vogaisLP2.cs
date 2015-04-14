44f4c00b6cbfccddc2176824ccc9bcffcfee4422
christianMOREIRA <christian.moreira.bertoldo@gmail.com>
2015-04-14 20:09:40 -0300

    att

---
+++ Exercicio/ExerciciosLP2_VOGAIS/vogaisLP2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            char[] vogais = {'a','e','i','o','u'};
+            int numvogal = 0;
+
+
+            Console.WriteLine("Digita uma mensagem");
+            texto = Console.ReadLine();
+
+            for (int i = 0; i < texto.Length; i++)
+            {
+                if (texto[i] == 'a')
+                {
+                    numvogal++;
+                }
+                if (texto[i] == 'e')
+                {
+                    numvogal++;
+                }
+
+                if (texto[i] == 'i')
+                {
+                    numvogal++;
+                }
+
+                if (texto[i] == 'o')
+                {
+                    numvogal++;
+                }
+
+                if (texto[i] == 'u')
+                {
+                    numvogal++;
+                }
+
+            }
+            Console.WriteLine("O número de vogais é {0}", numvogal); 
+         
+            
+        }
+    }
+}
