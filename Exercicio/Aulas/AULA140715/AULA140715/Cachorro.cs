e25c484faaeb9bf3257e9e8810a44e32fc2f2437
Christian <christian.moreira.bertoldo@hotmail.com>
2015-07-14 17:54:39 -0300

    aula140715

---
+++ Exercicio/Aulas/AULA140715/AULA140715/Cachorro.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AULA140715
+{
+    class Cachorro
+    {
+       
+        double RacaoDiariaKg;
+        private double _tamanhoCm;
+        private string _nome;
+        private double _peso;
+
+        public void SetPeso(double peso)
+        {
+            _peso = peso;
+        }
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+             
+        }
+        public void SettamanhoCm (double tamanhocm)
+        {
+            _tamanhoCm = tamanhocm;
+        }
+        public double RacaoDiariaKg(double Racao)
+        {
+            return Racao = (_peso / _tamanhoCm) / 10;
+            
+        }
+
+        public string GetNome(string nome)
+        {
+           return _nome;
+        }
+        public double GetPeso(int peso)
+        {
+            return _peso;
+        }
+        public double GetRacaoDiariaKg(double RacaoDiariaKg)
+        {
+            return RacaoDiariaKg;
+        }
+        public double GettamanhoCm (double tamanhoCm )
+        {
+            return _tamanhoCm;
+        }
+        }
+        
+    }
+}
---
+++ Exercicio/Aulas/AULA140715/AULA140715/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AULA140715
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro fido = new Cachorro();
+            fido.SetTamanhoCm(95);
+            fido.SetNome("fido");
+            fido.SetPeso(42.3);
+
+            Console.WriteLine("Nome: {0}", fido.GetNome());
+            Console.WriteLine("Peso: {0}", );
+            Console.WriteLine("Tamanho (m): {0}", );
+            Console.WriteLine("Quantidade diária de ração: {0}", fido.RacaoDiariaKg());
+
+
+        }
+    }
+}
